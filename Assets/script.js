d68587b5372c96e557b0f1d4c7314b07af0ab8dd
Annie Nguyen <anguyen.aln@gmail.com>
2021-11-21 09:24:51 -0800

    added calendar reset button with functionality and code to save task when added to planner

--- Assets/script.js
+++ Assets/script.js
@@ -6,29 +6,40 @@ setInterval(() => {
 $("#currentDay").text(now);
 }, 60000);
 
+function renderTasks() {
+    $("#9").val(localStorage.getItem("9"));
+    $("#10").val(localStorage.getItem("10"));
+    $("#11").val(localStorage.getItem("11"));
+    $("#12").val(localStorage.getItem("12"));
+    $("#1").val(localStorage.getItem("1"));
+    $("#2").val(localStorage.getItem("2"));
+    $("#3").val(localStorage.getItem("3"));
+    $("#4").val(localStorage.getItem("4"));
+    $("#5").val(localStorage.getItem("5"));
+}
+renderTasks()
 // //Add Task When clicking on button
 
 function addTask(e){
    var row_id= e.target.getAttribute("row")
     console.log(row_id);
+    console.log(e.target.attributes[0]);
+    console.log($("#"+row_id).val());
+    localStorage.setItem(row_id,$("#"+row_id).val());
 }
-
-
-function getValue() {
-    return localStorage.getItem("schedule");
-}
-console.log(getValue());
 
 $(".saveBtn").on("click", addTask);
 
 
 
-//clear calendar button
+//clear calendar button (reset)
 function ResetCal(){
-
+localStorage.clear()
+location.reload()
 }
 
-// $(".clearcal").accordion("ResetCal");
+$(".clearcal").on("click",ResetCal);
+
 
 //change to military time
 function mapToMilitary(time, am_pm){
@@ -68,3 +79,16 @@ function updateColors () {
 
 updateColors()
 
+// GIVEN I am using a daily planner to create a schedule
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar
+// WHEN I scroll down
+// THEN I am presented with timeblocks for standard business hours
+// WHEN I view the timeblocks for that day
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+// WHEN I click into a timeblock
+// THEN I can enter an event
+// WHEN I click the save button for that timeblock
+// THEN the text for that event is saved in local storage
+// WHEN I refresh the page
+// THEN the saved events persist
