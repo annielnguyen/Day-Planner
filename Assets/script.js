903c6b0218b3a7dc11e10758f71852f5d59782fa
Annie Nguyen <anguyen.aln@gmail.com>
2021-11-20 17:40:04 -0800

    added current date and time to header

--- Assets/script.js
+++ Assets/script.js
@@ -1,8 +1,18 @@
-// $(document).ready(function() {
-//     const currentDayEl = $("header #currentDay");
-// }
+// // $(document).ready(function() {
 
-$("#currentDay").text(moment().format("MMM Do, YYYY"));
+// // $("#currentDay").text(moment().format("MMM Do, YYYY"));
+// var dateToday = moment().format("MMM Do, YYYY");
+// $("currentDay").text(dateToday);
 
-//Add Task Button
-col-1-addButton.addEventListener("click", saveTask);
+// //Add Task Button
+// // function saveTask(){
+
+// // }
+
+// // $(".col-1 addButton").addEventListener("click", saveTask);
+var now = moment().format("MMM Do, YYYY, hh:mm A")
+$("#currentDay").text(now);
+setInterval(() => {
+    now = moment().format("MMM Do, YYYY, hh:mm A")
+$("#currentDay").text(now);
+}, 60000);
